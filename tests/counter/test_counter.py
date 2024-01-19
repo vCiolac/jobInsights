949393020e28f1342ee50ca91eda6612c95723ed
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-19 17:37:38 -0300

    fix Req.1

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,8 +4,14 @@ from src.pre_built.counter import count_ocurrences
 def test_counter():
     file_path = "data/jobs.csv"
     word_py = "Python"
+    word_py_upper = "PYTHON"
     word_js = "JavaScript"
+    word_js_upper = "JAVASCRIPT"
     result_py = count_ocurrences(file_path, word_py)
     result_js = count_ocurrences(file_path, word_js)
-    assert result_py == 47
-    assert result_js == 44
+    result_py_upper = count_ocurrences(file_path, word_py_upper)
+    result_js_upper = count_ocurrences(file_path, word_js_upper)
+    assert result_py == 1639
+    assert result_js == 122
+    assert result_py_upper == 1639
+    assert result_js_upper == 122
