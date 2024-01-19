74c16f24d6e3680cab2f73a93b3a66d794357c22
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-19 19:38:55 -0300

    Req.8

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -26,8 +26,31 @@ class ProcessSalaries(ProcessJobs):
 
         return min(valid_salaries)
 
+    def check_key(self, job: Dict, key: str) -> None:
+        if key not in job:
+            raise ValueError(f"A chave {key} está ausente no dicionário.")
+
+    def is_numeric(self, value: Union[int, str]) -> bool:
+        str_value = str(value)
+        return str_value.replace("-", "").isdigit()
+
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        mini = "min_salary"
+        maxi = "max_salary"
+
+        self.check_key(job, mini)
+        self.check_key(job, maxi)
+
+        if not self.is_numeric(job[mini]) or not self.is_numeric(job[maxi]):
+            raise ValueError(f"Valores de {mini} ou {maxi} não são numéricos.")
+
+        if int(job[mini]) > int(job[maxi]):
+            raise ValueError(f"Valor de {mini} é maior que o de {maxi}.")
+
+        if not self.is_numeric(salary):
+            raise ValueError("O valor do salário não é numérico.")
+
+        return int(job[mini]) <= int(salary) <= int(job[maxi])
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
