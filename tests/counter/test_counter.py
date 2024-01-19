2d9c1faa622809bd833faa0890ebc1cd85e1843e
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-19 17:31:50 -0300

    Req-1. acho que é isso

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    file_path = "data/jobs.csv"
+    word_py = "Python"
+    word_js = "JavaScript"
+    result_py = count_ocurrences(file_path, word_py)
+    result_js = count_ocurrences(file_path, word_js)
+    assert result_py == 47
+    assert result_js == 44
