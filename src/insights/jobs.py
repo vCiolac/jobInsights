0494e40baac8ca959e7bc6578fb28f900367e507
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-19 18:27:00 -0300

    fix blank line chato!

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -11,6 +11,7 @@ class ProcessJobs:
             csv_reader = csv.DictReader(file)
             for line in csv_reader:
                 self.jobs_list.append(line)
+
         return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
@@ -18,13 +19,21 @@ class ProcessJobs:
 
         for job in self.jobs_list:
             unique_job_types.add(job["job_type"])
+
         return list(unique_job_types)
 
-    def filter_by_multiple_criteria(self, jobs_list: List[Dict], filter_criteria: Dict) -> List[dict]:
+    def filter_by_multiple_criteria(
+        self,
+        jobs_list: List[Dict],
+        filter_criteria: Dict
+    ) -> List[dict]:
         filtered_jobs = list()
 
-        for job in jobs_list:
-            if job["job_type"] == filter_criteria["job_type"] and job["industry"] == filter_criteria["industry"]:
-                filtered_jobs.append(job)
-        return filtered_jobs
+        jtype = filter_criteria["job_type"]
+        ind = filter_criteria["industry"]
 
+        for job in jobs_list:
+            if job["job_type"] == jtype and job["industry"] == ind:
+                filtered_jobs.append(job)
+
+        return filtered_jobs
