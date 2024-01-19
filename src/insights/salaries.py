37eac06ff7afe8831a6809f338d15e08b27b1413
Victor Ciolac <victor.bcfraga@gmail.com>
2024-01-19 19:04:32 -0300

    Req. 6 e 7

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -6,11 +6,25 @@ class ProcessSalaries(ProcessJobs):
     def __init__(self):
         super().__init__()
 
-    def get_max_salary(self) -> int:
-        pass
+    def get_max_salary(self) -> [int]:
+        maxi = "max_salary"
+        valid_salaries = [
+            int(job[maxi])
+            for job in self.jobs_list
+            if job[maxi] not in ["", "None", None] and job[maxi].isdigit()
+        ]
+
+        return max(valid_salaries)
 
     def get_min_salary(self) -> int:
-        pass
+        mini = "min_salary"
+        valid_salaries = [
+            int(job[mini])
+            for job in self.jobs_list
+            if job[mini] not in ["", "None", None] and job[mini].isdigit()
+        ]
+
+        return min(valid_salaries)
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
